452c3c8e435c824b127dcae9a9ca57fa55cf9436
how <51109816+how223366@users.noreply.github.com>
2022-09-29 04:00:57 +0800

    Update qqvideo.js

--- rewrite/qqvideo.js
+++ rewrite/qqvideo.js
@@ -1,4 +1,4 @@
-hostname = act.vip.iqiyi.com
+hostname = *.tc.qq.com
 # > 腾讯视频广告屏蔽
 ^http:\/\/.+/\?tk=9901afb195dcf9a34db6088a4a221dba38b3d980e4db86009f8a08be4d4099ee323f6e7f03b881db21133b1bf2ae5bc5 url reject
 ^http:\/\/video\.dispatch\.tc\.qq\.com\/.+\.mp4 url reject
